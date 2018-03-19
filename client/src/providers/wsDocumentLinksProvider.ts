1e9e7a1d3fe6b0efc44f4a436aa3148d0d8114d3
Fabien Tencé <fabien.tence@cityzendata.com>
2018-03-19 16:57:02 +0100

    Fixed and enhanced the local referencing macros feature.

--- client/src/providers/wsDocumentLinksProvider.ts
+++ client/src/providers/wsDocumentLinksProvider.ts
@@ -3,29 +3,36 @@
 import { TextDocument, CancellationToken, workspace, DocumentLinkProvider, DocumentLink, Uri, Range } from 'vscode';
 
 export default class WSDocumentLinksProvider implements DocumentLinkProvider {
-    private _linkPattern = /([@][^\s]+)/g;
-    public static links: any = {};
+    private _linkPattern = /@([^\s]+)/g;
 
     public async provideDocumentLinks(document: TextDocument, _token: CancellationToken): Promise<DocumentLink[]> {
         const results: DocumentLink[] = []
         const text = document.getText();
-        WSDocumentLinksProvider.links = {};
         this._linkPattern.lastIndex = 0;
         let match: RegExpMatchArray | null;
         let macroNames = []
         while ((match = this._linkPattern.exec(text))) {
-            const pre = match[1];
+            const macroLinkName = match[0];
+            const macroName = match[1];
             const offset = match.index;
             const linkStart = document.positionAt(offset);
-            const linkEnd = document.positionAt(offset + pre.length);
-            macroNames.push(pre.replace('@', ''))
-            const doc = await workspace.findFiles('**/' + pre.replace('@', '') + '.mc2', '**/node_modules/**', 1)
-            if (doc[0]) {
-                let uri = Uri.parse('file:/' + doc[0].path);
-                WSDocumentLinksProvider.links[pre] = doc[0].path
-                results.push(new DocumentLink(new Range(linkStart, linkEnd), uri));
-            }
+            const linkEnd = document.positionAt(offset + macroLinkName.length);
+            macroNames.push(macroName)
+            await WSDocumentLinksProvider.getMacroURI(macroName).then(
+                (uri) => results.push(new DocumentLink(new Range(linkStart, linkEnd), uri))
+            );
         }
         return Promise.resolve(results);
+    }
+
+    public static async getMacroURI(macroName: string): Promise<Uri>{
+        return new Promise<Uri>(async (resolve, reject) => {
+            const doc = await workspace.findFiles('**/' + macroName + '.mc2', '**/node_modules/**', 1)
+            if (doc[0]) {
+                let uri = Uri.parse('file:/' + doc[0].path);
+                resolve(uri);
+            }
+            reject();
+        });
     }
 }
