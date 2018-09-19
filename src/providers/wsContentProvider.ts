f356c427b312d024304d31248b0ff4f83a4b58f8
Xavier Marin <marin.xavier@gmail.com>
2018-09-19 10:33:43 +0200

    Adding theme setting

--- src/providers/wsContentProvider.ts
+++ src/providers/wsContentProvider.ts
@@ -19,12 +19,31 @@ export default class WSContentProvider implements vscode.TextDocumentContentProv
             return `
 <script src="file://${rootPath + '/bower_components/quantum-viz/dist/quantumviz.js'}"></script>
 <style>
-    body { 
-        background-color: #fff; 
+    .container { 
+        background-color: #f8f9fa;
         color: #000; 
-    }
+        margin: 0;
+        font-family: -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol";
+        font-size: 1rem;
+        font-weight: 400;
+        line-height: 1.5;
+        color: #212529;
+        text-align: left;
+      }
+  
+      .container.dark {
+        background-color: #343a40;
+        --quantum-font-color: #ffffff;
+        --quantum-chart-label-color: #ffffff;
+        --gts-tree-expanded-icon: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAYAAAAeP4ixAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAACASURBVGhD7djRCYAwDEXRLOCHWzqKe7lXawIZILQEQ7wH+hl4D5SUCgAA+IMxxjHnPCsdy+Tx4nTo0eFSLJPHi6NIotUiPf4RAAB26PK5dAndlY5l8nhxdh3Q4VIsk8eL61Skx6cFAMAOXT480GWxTB4vjiKJVovwQAcAAD4h8gJ93ZLCEjQrYQAAAABJRU5ErkJggg==');
+        --gts-stack-font-color: #ffffff;
+        --quantum-switch-inset-color: #000000;
+        --quantum-switch-inset-checked-color: darkgreen;
+        --quantum-switch-handle-color: #8e8e8e;
+        --quantum-switch-handle-checked-color: lime;
+      }
 </style>
-<div class="container">
+<div class="container ${vscode.workspace.getConfiguration().get('warpscript.theme') === 'dark'? 'dark': ''}">
     <quantum-plot responsive="true" data='${this.currentDocument.getText()}' showLegend="false" ></quantum-plot>
 </div>`
         } else return '';
