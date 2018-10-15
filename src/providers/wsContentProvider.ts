668cac492c2e203698c205d59afbdee013543b3f
Xavier Marin <marin.xavier@gmail.com>
2018-10-15 16:47:51 +0200

    fix broken viz

--- src/providers/wsContentProvider.ts
+++ src/providers/wsContentProvider.ts
@@ -17,35 +17,16 @@ export default class WSContentProvider implements vscode.TextDocumentContentProv
         let rootPath = this.context.asAbsolutePath('.').replace(/\\/g, '/');
         if (this.currentDocument) {
             return `
-<script src="file://${rootPath + '/bower_components/quantum-viz/dist/quantumviz.js'}"></script>
+<script src="file://${rootPath + '/bower_components/webcomponentsjs/webcomponents-loader.js'}"></script>
+<link rel="import" href="file://${rootPath + '/bower_components/shadycss/apply-shim.html'}">
+<link rel="import" href="file://${rootPath + '/bower_components/warp10-quantum-elements/quantum-plot.html'}">
 <style>
-    .container { 
-        background-color: #f8f9fa;
+    body { 
+        background-color: #fff; 
         color: #000; 
-        margin: 0;
-        font-family: -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol";
-        font-size: 1rem;
-        font-weight: 400;
-        line-height: 1.5;
-        color: #212529;
-        text-align: left;
-      }
-  
-      .container.dark {
-        background-color: #343a40;
-        --quantum-font-color: #ffffff;
-        --quantum-chart-label-color: #ffffff;
-        --gts-tree-expanded-icon: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAYAAAAeP4ixAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAACASURBVGhD7djRCYAwDEXRLOCHWzqKe7lXawIZILQEQ7wH+hl4D5SUCgAA+IMxxjHnPCsdy+Tx4nTo0eFSLJPHi6NIotUiPf4RAAB26PK5dAndlY5l8nhxdh3Q4VIsk8eL61Skx6cFAMAOXT480GWxTB4vjiKJVovwQAcAAD4h8gJ93ZLCEjQrYQAAAABJRU5ErkJggg==');
-        --gts-stack-font-color: #ffffff;
-        --quantum-switch-inset-color: #000000;
-        --quantum-switch-inset-checked-color: darkgreen;
-        --quantum-switch-handle-color: #8e8e8e;
-        --quantum-switch-handle-checked-color: lime;
-      }
+    }
 </style>
-<div class="container ${vscode.workspace.getConfiguration().get('warpscript.theme') === 'dark'? 'dark': ''}">
-    <quantum-plot responsive="true" data='${this.currentDocument.getText()}' showLegend="false" ></quantum-plot>
-</div>`
+<div class="container"><quantum-plot name="plot" stack='${this.currentDocument.getText()}'></quantum-plot></div>`
         } else return '';
     }
 
