96ae5acc736e5493d83216bc7d87bab5d3669fd7
pi-r-p <pierre.papin@senx.io>
2018-12-12 21:36:32 +0100

    introduction of CloseEveryOpenedJsonOutput function

---
+++ src/features/closeJsonResults.ts
@@ -0,0 +1,68 @@
+import * as vscode from 'vscode';
+import WarpScriptExtConstants from '../constants';
+import WarpStriptExtGlobals = require('../globals')
+/**
+ * This functions browse all the opened documents.
+ * If the filename match the /tmp/xxxx.json pattern used for the WarpScript output, it close them without any confirmation.
+ * 
+ * Trick to browse opened files : we must focus on them to get the document properties. 
+ * So we trigger a workbench.action.nextEditor, then a callback onDidChangeActiveTextEditor.
+ * 
+ * In order not to mess with onDidChangeActiveTextEditor dedicated to the content provider refresh (in extension.ts), 
+ * there is global variable called weAreClosingFilesFlag
+ * 
+ * In case of a long loop or endless loop, there is a five second timeout security
+ */
+
+export default class CloseJsonResults {
+
+
+
+
+  public exec(): any {
+
+    //the current opened file. usefull to detect the end of "nextEditor" loop.
+    let activefilepath: string = vscode.window.activeTextEditor.document.uri.toString();
+    //because one loop is not enough :(
+    let firstLoop: boolean = true;
+
+
+    let closeIfOutputJson: Function = (editor: vscode.TextEditor) => {
+      let filename: string = editor.document.fileName;
+      if (filename.match(new WarpScriptExtConstants().jsonResultRegEx)) {
+        console.log(filename + ' could be closed ! closing...');
+        vscode.commands.executeCommand("workbench.action.closeActiveEditor");
+      }
+      else {
+        if (activefilepath === editor.document.uri.toString()) {
+          console.log('end of close cycle?');
+          if (firstLoop) {
+            vscode.commands.executeCommand("workbench.action.nextEditor");
+          } else {
+            if (onchangecallback) { onchangecallback.dispose(); } 
+            WarpStriptExtGlobals.weAreClosingFilesFlag = false;
+          }
+        } else {
+          vscode.commands.executeCommand("workbench.action.nextEditor");
+        }
+      }
+    }
+
+    // callback on change active editor (called after one is closed OR after nextEditor)
+    let onchangecallback: vscode.Disposable = vscode.window.onDidChangeActiveTextEditor(editor => { closeIfOutputJson(editor); });
+
+
+    //security : if endless loop (not seen), end all.
+    setTimeout(function () {
+      if (onchangecallback) { onchangecallback.dispose(); }
+      WarpStriptExtGlobals.weAreClosingFilesFlag = false;
+    }, 5000);
+
+    //set the flag to disable the content provider update
+    WarpStriptExtGlobals.weAreClosingFilesFlag = true;
+
+    //initiate the loop
+    closeIfOutputJson(vscode.window.activeTextEditor);
+
+  }
+}
---
+++ src/globals.ts
@@ -0,0 +1,10 @@
+/**
+ * Put here every global variables/flags of the project.
+ */
+
+ 
+/**
+ * When we launch the "close all json" command, this flag is set to true
+ * The content providers won't be updated as long as this flag is true.
+ */
+export let weAreClosingFilesFlag:boolean = false;
