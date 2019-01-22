2a509fd5ec7dd030e17d97b2a0ea2c9b7ea44f13
Xavier Marin <marin.xavier@gmail.com>
2019-01-22 10:25:28 +0100

    changelog

--- changelog.js
+++ changelog.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 const { execSync } = require('child_process');
-let repo = 'https://github.com/senx/VSCode-WarpScriptLanguage/';
+let repo = 'https://github.com/senx/VSCode-WarpScriptLanguage';
 let md =
   `VSCode WarpScript Extension
 ---
