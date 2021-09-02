a5d195503f65ad57177d5c834922e51f756a361d
pi-r-p <pierre.papin@senx.io>
2021-09-02 22:05:44 +0200

    json autoescape feature may break discovery. add an explicit failure and advice for user.

--- src/webviews/discoveryPreview.ts
+++ src/webviews/discoveryPreview.ts
@@ -1,13 +1,18 @@
-
+import * as vscode from 'vscode';
 export default class DiscoveryPreviewWebview {
 
   constructor() { }
 
-  public async findDiscoveryHtml(data: string): Promise<String> {
+  public async findDiscoveryHtml(data: string,outputWin:vscode.OutputChannel): Promise<String> {
     let discoveryHtml: String = "";
     if (data.startsWith('["<!DOCTYPE html>') && data.endsWith('</body></html>"]')) {
-      let objlist = JSON.parse(data);
-      discoveryHtml = objlist[0];
+      try {
+        let objlist = JSON.parse(data);
+        discoveryHtml = objlist[0];
+      } catch (error) {
+        outputWin.appendLine("Unable to parse json output for discovery. Try to disable unicode unescape feature: In the settings, set 'max File Size For Automatic Unicode Escape' to zero.")
+      }
+
     }
 
     console.log("found html that could be a discovery dashboard")
